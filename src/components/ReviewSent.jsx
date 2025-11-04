1645f861748364d1c4a980a729edea7e74d6e71f
Maryam <maryam.chaudhry@iths.se>
2025-11-04 10:09:23 +0100

    added new component

---
+++ src/components/ReviewSent.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+export function ReviewSent() {
+    return (
+        <>
+        <div>
+            <h2>Offert skickad!</h2>
+            <h3></h3>
+        </div>
+        </>
+    )
+}
