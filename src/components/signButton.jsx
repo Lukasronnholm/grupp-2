9c933211a7906cb9b029dc3cb361ec0f6de4f0f2
Lukas <lukas@macbookpro.home>
2025-11-06 13:04:00 +0100

    started signbutton

--- src/components/signButton.jsx
+++ src/components/signButton.jsx
@@ -1,9 +1,14 @@
 import {FiEdit3} from 'react-icons/fi';
-function SignButton({ onClick }) {
+import { useState } from 'react';
+import BankIDModal from './bankID';
+function SignButton() {
+    const [showBankID, setShowBankID] = useState(false);
   return (
-    <button onClick={onClick} >
+    <>
+    <button onClick={() => setShowBankID(true)}  >
      Signera Offert <FiEdit3 /> 
     </button>
-  );
-}
+    {showBankID && <BankIDModal />}
+    </>
+)}
 export default SignButton;
